7029d97dff69e2c02b768bd3c57297ebc72a1f1b
alexis-anderson <aanderson9313@gmail.com>
2020-02-16 21:23:09 -0500

    mvp some stretch

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
+
+const Friend = props => {
+    
+    return (
+
+        <>
+            <h2>Name: {props.data.name} </h2>
+            <p class = 'age'> Age: {props.data.age} </p>
+            <p class = 'email'> E-Mail: {props.data.email} </p>
+        </>
+    );
+}
+
+export default Friend;
